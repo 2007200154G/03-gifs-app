cfd9cff7a1867bc8d9c9eed9f9e98a247c58c599
JarCx9 <jarc.kun@gmail.com>
2024-08-08 11:10:17 -0500

    n

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -20,7 +20,7 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     SharedModule
   ],
   exports: [
-    HomePageComponent,
+    HomePageComponent
   ]
 })
 export class GifsModule { }
